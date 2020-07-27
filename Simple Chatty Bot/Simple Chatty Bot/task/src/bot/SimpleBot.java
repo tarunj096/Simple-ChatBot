468419e4ac4d3cbf36c34495574d9d8d25470ac6
tarunj096 <58319215+tarunj096@users.noreply.github.com>
2020-07-28 00:50:15 +0530

    Update

--- Simple Chatty Bot/Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -6,7 +6,7 @@ public class SimpleBot {
     final static Scanner scanner = new Scanner(System.in); // Do not change this line
 
     public static void main(String[] args) {
-        greet("Aid", "2018"); // change it as you need
+        greet("TAJ", "2020"); // change it as you need
         remindName();
         guessAge();
         count();
